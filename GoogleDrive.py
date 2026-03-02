9c5119292a9a05361e35a97dd937a8ecc3f06516
TimGorter <34648298+bobgithub12@users.noreply.github.com>
2026-03-02 14:42:58 +0100

    Update GoogleDrive.py

--- GoogleDrive.py
+++ GoogleDrive.py
@@ -115,7 +115,11 @@ class GoogleDrive:
                         download_file = True
                     if download_file:
                         # self.download(file['id'], file['name'], folder['name'])
-                        threading.Thread(target=self.download(file['id'], file['name'], folder['name'])).start()
+                        # threading.Thread(target=self.download(file['id'], file['name'], folder['name'])).start()
+                        threading.Thread(
+                            target=self.download,
+                            args=(file['id'], file['name'], folder['name'])
+                        ).start()
 
 
                     downloaded_files += 1
